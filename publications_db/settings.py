2d01112c0b3b9574c05607dd08cd370e716ae38f
Hermotimos <lukas.kozicki@gmail.com>
2019-10-15 14:21:26 +0200

    connected Django with MySQL

--- publications_db/settings.py
+++ publications_db/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'publications_db.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'publications',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
