6fcd64fb1a7f9bf121b02e03a25f2a2aaeb53128
Hermotimos <lukas.kozicki@gmail.com>
2019-10-15 14:40:12 +0200

    settings configured + *.pyc adde d to .gitignore

--- publications_db/settings.py
+++ publications_db/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # own
+    'bibliography'
 ]
 
 MIDDLEWARE = [
@@ -109,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
