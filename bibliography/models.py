b9c22db8fecbcc3a910acb20aaf44da83ea9050c
Hermotimos <lukas.kozicki@gmail.com>
2019-10-16 12:21:50 +0200

    models modified in bibliograpfy app to be flexible - less validation (after call with client)

--- bibliography/models.py
+++ bibliography/models.py
@@ -13,8 +13,7 @@ class Author(models.Model):
 
 
 class Location(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Miejscowość")
-    # TODO add [bmw] for 'brak miejsca wydania' for basic locations
+    name = models.CharField(max_length=500, verbose_name="Miejscowość")
 
     class Meta:
         verbose_name = 'Miejscowość'
@@ -22,29 +21,29 @@ class Location(models.Model):
 
 
 class BibliographicUnit(models.Model):
-    authorship_type = models.CharField(max_length=30, default='', verbose_name='Rodzaj autorstwa')
+    authorship = models.CharField(max_length=100, verbose_name='Rodzaj autorstwa (np. red., oprac.)', blank=True, null=True)
     authors = models.ManyToManyField(Author, related_name='bibliographic_units', verbose_name='Autor/Autorzy')
-    title = models.CharField(max_length=1000, verbose_name="Tytuł")
+    title = models.CharField(max_length=1000, verbose_name="Tytuł", blank=True, null=True)
 
     # case A: independent bibliographic unit:
     published_location = models.ManyToManyField(Location, related_name='bibliographic_units', verbose_name='Miejsce/miejsca wydania')
-    published_year = models.PositiveSmallIntegerField(verbose_name="Rok wydania")     # TODO add validators: min=1918, max=2099
-    volumes = models.CharField(max_length=10, blank=True, null=True, verbose_name="Tomy")
-    edition = models.CharField(max_length=100, blank=True, null=True, verbose_name="Wydanie")
+    published_year = models.CharField(max_length=100, verbose_name="Rok wydania", blank=True, null=True)
+    volumes = models.CharField(max_length=100, verbose_name="Tomy", blank=True, null=True)
+    edition = models.CharField(max_length=100, verbose_name="Wydanie", blank=True, null=True)
     # TODO: ask what else can possible get here
 
     # case B: published within an independent bibliograhic unit:
     # TODO published_in = models.ManyToManyField(BibliographicUnit, related_name='dependent_bibliographic_units', blank=True, null=True, verbose_name='Opublikowane w:')
     # TODO this should relate to itself: https://stackoverflow.com/questions/11721157/django-many-to-many-m2m-relation-to-same-model
-    # PROBABLY THIS WILL DO: published_in = models.ManyToManyField('self', related_name='dependent_bibliographic_units', blank=True, null=True, verbose_name='Opublikowane w:')
+    published_in = models.ManyToManyField('self', related_name='dependent_bibliographic_units', blank=True, null=True, verbose_name='Opublikowane w:')
 
     # case C: published within a periodical bibliographic unit:
-    periodical_pub_title = models.CharField(max_length=1000, verbose_name="Tytuł periodyku")
-    periodical_pub_year = models.PositiveSmallIntegerField(verbose_name="Rok wydania")     # TODO add validators: min=1918, max=2099
-    periodical_pub_vol_lvl_1 = models.CharField(max_length=10, verbose_name="Numeracja wolumenu poziom 1")  # TODO make verbose_names more precise - consult client
-    periodical_pub_vol_lvl_2 = models.CharField(max_length=10, verbose_name="Numeracja wolumenu poziom 2")  # TODO make verbose_names more precise - consult client
-    periodical_pub_vol_lvl_3 = models.CharField(max_length=10, verbose_name="Numeracja wolumenu poziom 3")  # TODO make verbose_names more precise - consult client
-    periodical_pub_pages = models.CharField(max_length=20, verbose_name="Strony")
+    periodical_pub_title = models.CharField(max_length=1000, verbose_name="Tytuł periodyku", blank=True, null=True)
+    periodical_pub_year = models.CharField(max_length=100, verbose_name="Rocznik", blank=True, null=True)     
+    periodical_pub_vol_lvl_1 = models.CharField(max_length=100, verbose_name="Numeracja wolumenu poziom 1", blank=True, null=True)  # TODO make verbose_names more precise - consult client
+    periodical_pub_vol_lvl_2 = models.CharField(max_length=100, verbose_name="Numeracja wolumenu poziom 2", blank=True, null=True)  # TODO make verbose_names more precise - consult client
+    periodical_pub_vol_lvl_3 = models.CharField(max_length=100, verbose_name="Numeracja wolumenu poziom 3", blank=True, null=True)  # TODO make verbose_names more precise - consult client
+    periodical_pub_pages = models.CharField(max_length=100, verbose_name="Strony", blank=True, null=True)
 
     class Meta:
         verbose_name = 'Opis bibliograficzny'
