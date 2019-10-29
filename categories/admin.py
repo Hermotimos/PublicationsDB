8f1edd934ed4da779a3d428910fe7ea613b2b6f8
Hermotimos <lukas.kozicki@gmail.com>
2019-10-29 15:41:50 +0100

    commented out removed models

--- categories/admin.py
+++ categories/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Category, SubcategoryLevelOne, SubcategoryLevelTwo, SubcategoryLevelThree, SubcategoryLevelFour
+from .models import Category, SubcategoryLevelOne, SubcategoryLevelTwo
+# SubcategoryLevelThree, SubcategoryLevelFour
+
 
 admin.site.register(Category)
 admin.site.register(SubcategoryLevelOne)
 admin.site.register(SubcategoryLevelTwo)
-admin.site.register(SubcategoryLevelThree)
-admin.site.register(SubcategoryLevelFour)
+# admin.site.register(SubcategoryLevelThree)
+# admin.site.register(SubcategoryLevelFour)
