e4757b23f620c06200f071ab7ceab8b7cefeaf54
Hermotimos <lukas.kozicki@gmail.com>
2019-10-30 10:35:32 +0100

    removed remnants of old code from admin.py in bibliography app

--- bibliography/admin.py
+++ bibliography/admin.py
@@ -2,30 +2,6 @@ from django.contrib import admin
 from .models import BibliographicUnitBook, BibliographicUnitPartOfBook, BibliographicUnitPartOfPeriodical
 
 
-class BibliographicUnitBookAdmin(admin.ModelAdmin):
-    def save_related(self, request, form, formsets, change):
-        form.save_m2m()
-        # for formset in formsets:                                      # TODO seems to be redundant - but wait and see
-        #     self.save_formset(request, form, formset, change=change)
-        super(BibliographicUnitBookAdmin, self).save_model(request, form.instance, form, change)
-
-
-class BibliographicUnitPartOfBookAdmin(admin.ModelAdmin):
-    def save_related(self, request, form, formsets, change):
-        form.save_m2m()
-        # for formset in formsets:
-        #     self.save_formset(request, form, formset, change=change)
-        super(BibliographicUnitPartOfBookAdmin, self).save_model(request, form.instance, form, change)
-
-
-class BibliographicUnitPartOfPeriodicalAdmin(admin.ModelAdmin):
-    def save_related(self, request, form, formsets, change):
-        form.save_m2m()
-        # for formset in formsets:
-        #     self.save_formset(request, form, formset, change=change)
-        super(BibliographicUnitPartOfPeriodicalAdmin, self).save_model(request, form.instance, form, change)
-
-
-admin.site.register(BibliographicUnitBook, BibliographicUnitBookAdmin)
-admin.site.register(BibliographicUnitPartOfBook, BibliographicUnitPartOfBookAdmin)
-admin.site.register(BibliographicUnitPartOfPeriodical, BibliographicUnitPartOfPeriodicalAdmin)
+admin.site.register(BibliographicUnitBook)
+admin.site.register(BibliographicUnitPartOfBook)
+admin.site.register(BibliographicUnitPartOfPeriodical)
