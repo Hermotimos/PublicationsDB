8de4178eded7889a1571ee3b5f8d70ce1a63a347
Hermotimos <lukas.kozicki@gmail.com>
2019-10-31 16:33:44 +0100

    Django admin forms customized for models in bibliography app

--- bibliography/admin.py
+++ bibliography/admin.py
@@ -1,7 +1,85 @@
 from django.contrib import admin
 from .models import BibliographicUnitBook, BibliographicUnitPartOfBook, BibliographicUnitPartOfPeriodical
+from django import forms
 
 
-admin.site.register(BibliographicUnitBook)
-admin.site.register(BibliographicUnitPartOfBook)
-admin.site.register(BibliographicUnitPartOfPeriodical)
+class BibliographicUnitBookAdminForm(forms.ModelForm):
+    class Meta:
+        model = BibliographicUnitBook
+        exclude = []
+        widgets = {
+            'authors': forms.SelectMultiple(attrs={'size': 20}),
+            'editors': forms.SelectMultiple(attrs={'size': 20}),
+            'translators': forms.SelectMultiple(attrs={'size': 20}),
+            'published_locations': forms.SelectMultiple(attrs={'size': 20}),
+            'cat_lvl_3': forms.SelectMultiple(attrs={'size': 20}),
+
+            'title': forms.TextInput(attrs={'size': 80}),
+            'annotation': forms.Textarea(attrs={'rows': 2, 'cols': 50}),
+            'sorting_name': forms.TextInput(attrs={'size': 80}),
+            'description': forms.TextInput(attrs={'size': 80}),
+
+            'editors_abbrev': forms.TextInput(attrs={'rows': 1, 'cols': 5}),
+            'translators_abbrev': forms.TextInput(attrs={'rows': 1, 'cols': 5}),
+            'published_year': forms.TextInput(attrs={'rows': 1, 'cols': 5}),
+            'volumes': forms.TextInput(attrs={'rows': 1, 'cols': 5}),
+            'edition': forms.TextInput(attrs={'rows': 1, 'cols': 5}),
+        }
+
+
+class BibliographicUnitPartOfBookAdminForm(forms.ModelForm):
+    class Meta:
+        model = BibliographicUnitBook
+        exclude = []
+        widgets = {
+            'authors': forms.SelectMultiple(attrs={'size': 20}),
+            'editors': forms.SelectMultiple(attrs={'size': 20}),
+            'translators': forms.SelectMultiple(attrs={'size': 20}),
+            'cat_lvl_3': forms.SelectMultiple(attrs={'size': 20}),
+
+            'title': forms.TextInput(attrs={'size': 80}),
+            'annotation': forms.Textarea(attrs={'rows': 2, 'cols': 50}),
+            'sorting_name': forms.TextInput(attrs={'size': 80}),
+            'description': forms.TextInput(attrs={'size': 80}),
+
+            'editors_abbrev': forms.TextInput(attrs={'rows': 1, 'cols': 5}),
+            'translators_abbrev': forms.TextInput(attrs={'rows': 1, 'cols': 5}),
+            'in_volume': forms.TextInput(attrs={'rows': 1, 'cols': 5}),
+        }
+
+
+class BibliographicUnitPartOfPeriodicalAdminForm(forms.ModelForm):
+    class Meta:
+        model = BibliographicUnitBook
+        exclude = []
+        widgets = {
+            'authors': forms.SelectMultiple(attrs={'size': 20}),
+            'editors': forms.SelectMultiple(attrs={'size': 20}),
+            'translators': forms.SelectMultiple(attrs={'size': 20}),
+            'cat_lvl_3': forms.SelectMultiple(attrs={'size': 20}),
+
+            'title': forms.TextInput(attrs={'size': 80}),
+            'annotation': forms.Textarea(attrs={'rows': 2, 'cols': 50}),
+            'sorting_name': forms.TextInput(attrs={'size': 80}),
+            'description': forms.TextInput(attrs={'size': 80}),
+
+            'editors_abbrev': forms.TextInput(attrs={'rows': 1, 'cols': 5}),
+            'translators_abbrev': forms.TextInput(attrs={'rows': 1, 'cols': 5}),
+        }
+
+
+class BibliographicUnitBookAdmin(admin.ModelAdmin):
+    form = BibliographicUnitBookAdminForm
+
+
+class BibliographicUnitPartOfBookAdmin(admin.ModelAdmin):
+    form = BibliographicUnitPartOfBookAdminForm
+
+
+class BibliographicUnitPartOfPeriodicalAdmin(admin.ModelAdmin):
+    form = BibliographicUnitPartOfPeriodicalAdminForm
+
+
+admin.site.register(BibliographicUnitBook, BibliographicUnitBookAdmin)
+admin.site.register(BibliographicUnitPartOfBook, BibliographicUnitPartOfBookAdmin)
+admin.site.register(BibliographicUnitPartOfPeriodical, BibliographicUnitPartOfPeriodicalAdmin)
